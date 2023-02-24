942bb4664e0128f824df8c8932a5dc4f6ec3a3c1
sai <saiforceone@gmail.com>
2023-02-24 01:01:11 -0500

    - add storybook install step to scaffold for React - update npm scripts for storybook if option is used - update npm attributes like name to match project

--- src/constants/feConstants.js
+++ src/constants/feConstants.js
@@ -1,3 +1,11 @@
 export const DIRT_PROJECT_FOLDER_NAME = '@dirt_project';
 
 export const DIRT_PROJECT_CONFIG_FILE_NAME = 'dirt.json';
+
+export const STORYBOOK_SCRIPT_DEV_PRE =
+  'tailwindcss -i ./static/css/main.css -o ./static/dist/css/app.css';
+export const STORYBOOK_SCRIPT_DEV =
+  'start-storybook -p 6006 --disable-telemetry';
+export const STORYBOOK_SCRIPT_BUILD = 'build-storybook';
+
+export const PACKAGE_JSON_FILE = 'package.json';
--- templates/react-templates/dirt_fe_react/src/components/shared/Note/Note.tsx
+++ templates/react-templates/dirt_fe_react/src/components/shared/Note/Note.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import type { FC } from 'react';
+import * as React from 'react';
 import { FaInfoCircle } from 'react-icons/all';
 
 interface NoteProps {
