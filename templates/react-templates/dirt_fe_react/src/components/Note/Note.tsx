ad941ab1a45bbf409da3d504c17415483e576bce
sai <saiforceone@gmail.com>
2023-02-23 21:15:30 -0500

    - add Note component

---
+++ templates/react-templates/dirt_fe_react/src/components/Note/Note.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import type { FC } from 'react';
+import { FaInfoCircle } from 'react-icons/all';
+
+interface NoteProps {
+  iconElement?: React.ReactNode;
+  labelText: string;
+  content: string;
+}
+
+export const Note: FC<NoteProps> = ({ iconElement, labelText, content }) => {
+  return (
+    <div className="flex items-center gap-x-2">
+      {iconElement ? iconElement : <FaInfoCircle size={20} />}
+      <span>
+        <span className="underline italic">{labelText}</span>{' '}
+        <span className="text-purple-800">{content}</span>
+      </span>
+    </div>
+  );
+};
