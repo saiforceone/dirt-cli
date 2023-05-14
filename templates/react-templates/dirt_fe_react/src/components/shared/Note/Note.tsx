293b84fc943b4fc2b1d7700ff90eb29dd413bbef
sai <saiforceone@gmail.com>
2023-05-14 14:04:22 -0500

    update icons and layout details to be consistent with templating in Vue

--- templates/react-templates/dirt_fe_react/src/components/shared/Note/Note.tsx
+++ templates/react-templates/dirt_fe_react/src/components/shared/Note/Note.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import * as React from 'react';
-import { FaInfoCircle } from 'react-icons/all';
+import { HiInformationCircle } from 'react-icons/hi2';
 
 interface NoteProps {
   iconElement?: React.ReactNode;
@@ -11,7 +11,7 @@ interface NoteProps {
 export const Note: FC<NoteProps> = ({ iconElement, labelText, content }) => {
   return (
     <div className="flex items-center gap-x-2">
-      {iconElement ? iconElement : <FaInfoCircle size={20} />}
+      {iconElement ? iconElement : <HiInformationCircle size={16} />}
       <div className="flex flex-col justify-items-start text-left md:flex-row md:items-center md:gap-x-2">
         <span className="underline italic">{labelText}</span>{' '}
         <span className="text-slate-400">{content}</span>
