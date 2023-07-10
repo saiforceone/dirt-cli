ce6d6218b1a977564c0093840e26b8f14a14c44c
sai <saiforeonce@gmail.com>
2023-07-09 22:48:43 -0500

    implement command (with alias) to generate secret key

--- src/advancedCli.ts
+++ src/advancedCli.ts
@@ -1,6 +1,7 @@
 import { Command } from '@commander-js/extra-typings';
 import ConsoleLogger from './utils/ConsoleLogger.js';
 import { checkDirt, cliInfo } from './advancedCommand.js';
+import { generateSecretKey } from './utils/generateSecretKey.js';
 
 // Main program instance
 const program = new Command();
@@ -20,6 +21,7 @@ program
   });
 
 /**
+ * todo: refactor / change from using exec
  * @description Exec command that runs advanced CLI commands
  */
 program
@@ -39,6 +41,20 @@ program
     ConsoleLogger.printMessage(
       `This command was called with [${str.createController}] and options: ${options} but this is just a placeholder`
     );
+  });
+
+/**
+ * @description Generate a secret key and print to the console / terminal
+ */
+program
+  .command('gen-key')
+  .alias('generate-key')
+  .description(
+    'Generates a pseudo-random string that can be used as a secret key'
+  )
+  .action(() => {
+    const key = generateSecretKey();
+    ConsoleLogger.printMessage(`Generated Key: ${key}`, 'success');
   });
 
 export default program;
